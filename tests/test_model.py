cf31211b194d7f8444e01e860fce149a04e55103
VijayDua <duavijay81@gmail.com>
2025-07-30 12:52:22 +0530

    787 cicd

--- tests/test_model.py
+++ tests/test_model.py
@@ -39,9 +39,9 @@ class TestModelLoading(unittest.TestCase):
         cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
 
     @staticmethod
-    def get_latest_model_version(model_name, stage="Staging"):
+    def get_latest_model_version(model_name):
         client = mlflow.MlflowClient()
-        latest_version = client.get_latest_versions(model_name, stages=[stage])
+        latest_version = client.get_latest_versions(model_name)
         return latest_version[0].version if latest_version else None
 
     def test_model_loaded_properly(self):
