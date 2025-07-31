4020c4b00f27ce508e0f8b3c601dd40b6e6cac8f
VijayDua <duavijay81@gmail.com>
2025-07-31 09:34:42 +0530

    NewA CICD

--- tests/test_model1.py
+++ tests/test_model1.py
@@ -20,25 +20,15 @@ class TestModelLoading(unittest.TestCase):
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
         dagshub_url = "https://dagshub.com"
-        repo_owner = "VijayDua"
-        repo_name = "Final _Project-MLOPS"
+        repo_owner = "vikashdas770"
+        repo_name = "YT-Capstone-Project"
 
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-        from mlflow.tracking import MlflowClient
-
-        cls.new_model_name = "my_model"
-        # Get all versions of the registered model
-        client = mlflow.MlflowClient()
-        versions = client.get_latest_versions(cls.new_model_name, stages=["None", "Staging", "Production", "Archived"])
-        
-        # Find the latest version by version number
-        latest_version = max(versions, key=lambda v: int(v.version))
-        cls.new_model_version = latest_version.version
-
         # Load the new model from MLflow model registry
-        
+        cls.new_model_name = "my_model"
+        cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
         cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
 
@@ -47,6 +37,12 @@ class TestModelLoading(unittest.TestCase):
 
         # Load holdout test data
         cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
+
+    @staticmethod
+    def get_latest_model_version(model_name, stage="Staging"):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=[stage])
+        return latest_version[0].version if latest_version else None
 
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
